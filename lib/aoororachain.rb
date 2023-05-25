38c7db304c1825a75e749a0e14e33222b94da397
Mario Alberto Chávez <mario.chavez@gmail.com>
2023-05-24 19:27:41 -0600

    Gem bootstrap

---
+++ lib/aoororachain.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "aoororachain/version"
+
+module Aoororachain
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/aoororachain/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Aoororachain
+  VERSION = "0.1.0"
+end
